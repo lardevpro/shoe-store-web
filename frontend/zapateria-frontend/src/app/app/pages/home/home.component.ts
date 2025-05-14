c3a5a1d1aa4ba474ad57ec26b589359add7dd57e
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-05-14 20:57:58 +0200

    feat: implement image carousel on home page with custom store images

--- frontend/zapateria-frontend/src/app/app/pages/home/home.component.ts
+++ frontend/zapateria-frontend/src/app/app/pages/home/home.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IvyCarouselModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   images = [
-    { path: 'images/tienda1.jpg' },
-    { path: 'images/tienda2.jpg' },
-    { path: 'images/tienda3.jpg' }
+    'images/imagesStore/fachada1.jpeg',
+    'images/imagesStore/fachada2.jpeg',
+    'images/imagesStore/fachada3.jpeg'
   ];
+  current = 0;
+
+  prev() {
+    this.current = (this.current - 1 + this.images.length) % this.images.length;
+  }
+  next() {
+    this.current = (this.current + 1) % this.images.length;
+  }
 }
