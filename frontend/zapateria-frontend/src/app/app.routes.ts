ebc35766bcd5cb81f5663a7921278bb6aa17ea69
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-05-21 17:53:39 +0200

    fix: delete deprecated contact component to replace routes and imports to FormComponent

--- frontend/zapateria-frontend/src/app/app.routes.ts
+++ frontend/zapateria-frontend/src/app/app.routes.ts
@@ -4,18 +4,19 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './app/pages/home/home.component';
 import { CatalogComponent } from './app/pages/catalog/catalog.component';
 import { AboutComponent } from './app/pages/about/about.component';
-import { ContactComponent } from './app/pages/contact/contact.component';
+import { Form } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoesComponent } from './app/components/shoe-card/shoes.components';
 import { BagsComponent } from './app/components/bag-card/bags.component';
 import { AccessoriesComponent } from './app/components/accesories-card/accesories.component';
+import { FormComponent } from './app/components/form/form.component';
 
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent }, // <--- coma agregada aquí
+  { path: 'contact', component: FormComponent }, // <--- coma agregada aquí
   {
     path: 'catalog',
     component: CatalogComponent,
